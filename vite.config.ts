750c1e9217b83ac413b66a627591639936d7e129
John Viscel M. Sangkal <jvmsangkal@gmail.com>
2026-05-02 15:37:47 +0800

    Setup oxfmt

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,35 @@
-import { defineConfig } from "vite-plus";
-import { devtools } from "@tanstack/devtools-vite";
-
-import { tanstackStart } from "@tanstack/react-start/plugin/vite";
-
-import viteReact, { reactCompilerPreset } from "@vitejs/plugin-react";
+import { cloudflare } from "@cloudflare/vite-plugin";
 import babel from "@rolldown/plugin-babel";
 import tailwindcss from "@tailwindcss/vite";
-import { cloudflare } from "@cloudflare/vite-plugin";
+import { devtools } from "@tanstack/devtools-vite";
+import { tanstackStart } from "@tanstack/react-start/plugin/vite";
+import viteReact, { reactCompilerPreset } from "@vitejs/plugin-react";
+import { defineConfig } from "vite-plus";
 
 const config = defineConfig({
   staged: {
     "*": "vp check --fix",
   },
-  fmt: {},
+  fmt: {
+    ignorePatterns: ["dist/**", "*.min.js"],
+    sortTailwindcss: {
+      stylesheet: "./src/styles.css",
+      attributes: ["class", "className"],
+      functions: ["clsx", "cn"],
+      preserveWhitespace: true,
+    },
+    sortImports: {
+      groups: [
+        "type-import",
+        ["value-builtin", "value-external"],
+        "type-internal",
+        "value-internal",
+        ["type-parent", "type-sibling", "type-index"],
+        ["value-parent", "value-sibling", "value-index"],
+        "unknown",
+      ],
+    },
+  },
   lint: { options: { typeAware: true, typeCheck: true } },
   resolve: { tsconfigPaths: true },
   plugins: [
